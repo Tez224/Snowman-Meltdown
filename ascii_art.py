54fd8715385abbdeb39dd46da52302d84ef2738a
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-07-03 10:27:20 +0200

    add new stages for smoother meltdown

--- ascii_art.py
+++ ascii_art.py
@@ -14,22 +14,49 @@ STAGES = [
      /___\\ 
      (o o) 
      ( : ) 
+     ( ' ) 
      """,
-     # Stage 2: Only the head remains
+     # Stage 2: Base shrinking (2 mistakes)
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 3: middle starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( . ) 
+     """,
+    # Stage 4: Only face and head left
      """
       ___  
      /___\\ 
      (o o)  
      """,
-     # Stage 3: Eyes fell off
+     # Stage 5: Eyes fell off
      """
       ___  
      /___\\ 
-     (   ) 
+     (o .  
      """,
-     # Stage 4: Snowman completely melted
+    # Stage 6: Features fading
+        """
+      ___  
+     /___\\ 
+     ( . .  
+     """,
+    # Stage 7: Nearly melted
      """
       ___  
-     /___\\
+     /   \\ 
+     '   ' 
+     """,
+    # Stage 8: Puddle remains
+     """
+     . . . 
+      ' ' 
      """
  ]
